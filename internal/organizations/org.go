488c383f27a3f11b79b83f6f105c743b278c682f
Douglas Fils <drfils@gmail.com>
2021-02-09 07:18:43 -0600

    start of org work in dev branch

--- internal/organizations/org.go
+++ internal/organizations/org.go
@@ -3,6 +3,7 @@ package organizations
 import "fmt"
 
 // BuildGRaph makes a graph from the Gleaner config file source
+// load this to a /sources bucket (change this to sources naming convention?)
 func BuildGraph() {
 	fmt.Println("vim-go")
 }
