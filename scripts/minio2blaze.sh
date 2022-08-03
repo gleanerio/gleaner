5ac3d29585d4f1ec06347a68e5f9e0115a10c505
Douglas Fils <drfils@gmail.com>
2022-08-03 14:57:00 -0500

    more work on context fix for context arrays

--- scripts/minio2blaze.sh
+++ scripts/minio2blaze.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 # A wrapper script for loading RDF into Blazegraph from Minio
-# usage:  load2blaze.sh souceBucket sparqlEndpoint
-# example: nas/gleaner-oih/summoned/cioos/  http://localhost:9999/blazegraph/namespace/kb/sparql
+# usage:  load2blaze.sh souceBucket
 
 mc_cmd() {
-        mc ls $1 | awk '{print $6}'
+        mc ls $1 | awk '{print $5}'
 }
-
-#if [ "$#" -ne 1  ]; then
-        #echo "Illegal number of parameters"
-#fi
 
 # If you use this for ntriples, be sure to add in a graph in the URL target
 for i in $(mc_cmd $1); do
       echo "-------------start-------------"
       echo Next: $i
-      mc cat $1/$i | curl -X POST -H 'Content-Type:text/x-nquads' --data-binary  @- $2
+      mc cat $1/$i | curl -X POST -H 'Content-Type:text/x-nquads' --data-binary  @- http://localhost:9999/blazegraph/namespace/kb/sparql
       echo "-------------done--------------"
 done
 
