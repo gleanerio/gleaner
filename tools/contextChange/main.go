48d4170c6d60ee5e051add39f5286c1ae93a2bd6
Douglas Fils <drfils@gmail.com>
2022-08-04 15:29:52 -0500

    working example now..   fixed issue with sjon and have test examples

--- tools/contextChange/main.go
+++ tools/contextChange/main.go
@@ -12,9 +12,9 @@ import (
 
 // Simple test of the nanoprov function
 func main() {
-	// Read file
-	//dat, err := os.ReadFile("./data/cioos.json")
-	dat, err := os.ReadFile("./data/sprep.json")
+
+	arg := os.Args[1]
+	dat, err := os.ReadFile(arg)
 	if err != nil {
 		fmt.Println("Error reading file")
 	}
@@ -56,67 +56,77 @@ func fixContextUrl(jsonld string) (string, error) {
 	// check to see if we can cast this to a map
 	cm, ok := c.Value().(map[string]interface{})
 	if !ok {
+		fmt.Println("-----  string context -------")
+		fmt.Println(c.Value().(string))
 		// if not it's a string and we can just regex it..  let's not promote to map
 		// check for https://schema.org/   (trailing / ?)  and fix
-		if strings.Contains(c.Value().(string), "http://scheme.org/") {
-			if (sdoc == "http://schema.org/") {
-				return jsonld, nil  // all good, return
+		if  c.Value().(string) == "http://schema.org/"  {
+			if strings.Compare(sdoc, "http://schema.org/") == 0 {
+				return jsonld, nil // all good, return
 			} else {
-				strings.Replace(c.Value().(string), "http://schema.org/", sdoc, 1)
+				jsonld, err = sjson.Set(jsonld, "@context", sdoc)
 				return jsonld, nil
 			}
-		} else if (strings.Contains(c.Value().(string), "https://scheme.org/") ) {
-			if (sdoc == "https://schema.org/") {
-				return jsonld, nil  // all good, return
+		} else if  c.Value().(string) == "https://schema.org/" {
+			if strings.Compare(sdoc, "https://schema.org/") == 0 {
+				return jsonld, nil // all good, return
 			} else {
-				strings.Replace(c.Value().(string), "https://schema.org/", sdoc, 1)
+				jsonld, err = sjson.Set(jsonld, "@context", sdoc)
 				return jsonld, nil
 			}
 		}
 
 		// repeat the above with the error of a missing trailing /
-		if strings.Contains(c.Value().(string), "http://scheme.org") {
-			if (sdoc == "http://schema.org/") {
-				return jsonld, nil  // all good, return
+		// check for this second so we don't match the shorter substring pattern first, if the string
+		// with the / is there we want to find it and leave this function first.
+		if c.Value().(string) == "http://schema.org" {
+			if strings.Compare(sdoc, "http://schema.org") == 0 {
+				return jsonld, nil // all good, return
 			} else {
-				strings.Replace(c.Value().(string), "http://schema.org", sdoc, 1)
+				jsonld, err = sjson.Set(jsonld, "@context", sdoc)
 				return jsonld, nil
 			}
-		} else if (strings.Contains(c.Value().(string), "https://scheme.org") ) {
-			if (sdoc == "https://schema.org/") {
-				return jsonld, nil  // all good, return
+		} else if  c.Value().(string) == "https://schema.org" {
+			if strings.Compare(sdoc, "https://schema.org") == 0 {
+				return jsonld, nil // all good, return
 			} else {
-				strings.Replace(c.Value().(string), "https://schema.org", sdoc, 1)
+				jsonld, err = sjson.Set(jsonld, "@context", sdoc)
 				return jsonld, nil
 			}
 		}
-	}
-	for k, v := range cm {
-		fmt.Printf("Key: %s  Value: %s\n", k, v)
+	} else {
+		for k, v := range cm {
+			fmt.Printf("Key: %s  Value: %s\n", k, v)
 
-		// if value is our schema.org issue then update this key
-		// try to ready this for a "config"  option
+			// if value is our schema.org issue then update this key
+			// try to ready this for a "config"  option
 
-		// NOTE we are checking the namespace as a prefix WITHOUT the training / to check
-		// on it too in the logic
-		if strings.HasPrefix(v.(string), "https://schema.org") {
-			if v.(string) == sdoc {
-				// we are good..  including trailing / as well, so leave
-				return jsonld, nil
-			} else {
-				tns := fmt.Sprintf("@context.%s", k)
-				jsonld, err = sjson.Set(jsonld, tns, sdoc)
-				return jsonld, err
-			}
-		} else if strings.HasPrefix(v.(string), "http://schema.org") {
-			if v.(string) == sdoc {
-				// we are good..  including trailing / as well, so leave
-				return jsonld, nil
-			} else {
-				tns := fmt.Sprintf(`@context.%s`, k) ////@context/@vocab
-				fmt.Printf("CHECK MARK:  _%s_  _%s_", tns, sdoc)
-				jsonld, err = sjson.Set(jsonld, tns, sdoc)
-				return jsonld, err
+			// NOTE we are checking the namespace as a prefix WITHOUT the training / to check
+			// on it too in the logic
+			if strings.HasPrefix(v.(string), "https://schema.org") {
+				if v.(string) == sdoc {
+					// we are good..  including trailing / as well, so leave
+					return jsonld, nil
+				} else {
+					tns := fmt.Sprintf("@context.%s", k)
+					jsonld, err = sjson.Set(jsonld, tns, sdoc)
+					return jsonld, err
+				}
+			} else if strings.HasPrefix(v.(string), "http://schema.org") {
+				tns := ""
+				if v.(string) == sdoc {
+					// we are good..  including trailing / as well, so leave
+					return jsonld, nil
+				} else {
+					if strings.HasPrefix(k, "@") {
+						tns = fmt.Sprintf("@context.\\%s", k) ////@context/@vocab
+					} else {
+						tns = fmt.Sprintf("@context.%s", k) ////@context/@vocab
+					}
+					fmt.Printf("CHECK MARK:%s:%s\n", tns, sdoc)
+					jsonld, err = sjson.Set(jsonld, tns, sdoc)
+					return jsonld, err
+				}
 			}
 		}
 	}
