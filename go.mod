9a08e847cff13b680efc4b7ee91e1c53038bf64b
Douglas Fils <drfils@gmail.com>
2022-08-04 12:38:51 -0500

    testing context updater with sjon and a depbot update as well.

--- go.mod
+++ go.mod
@@ -34,7 +34,7 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/temoto/robotstxt v1.1.2
 	github.com/tidwall/gjson v1.14.1
-	github.com/tidwall/sjson v1.2.3
+	github.com/tidwall/sjson v1.2.4
 	github.com/utahta/go-openuri v0.1.0
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1
 	google.golang.org/api v0.60.0
--- tools/contextChange/main.go
+++ tools/contextChange/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/url"
 	"os"
 	"strings"
 
@@ -13,53 +12,115 @@ import (
 
 // Simple test of the nanoprov function
 func main() {
-
 	// Read file
-	fmt.Println("Read in a JSON-LD data graph and test changing its context")
-	dat, err := os.ReadFile("../../secret/data/sprep.json")
+	//dat, err := os.ReadFile("./data/cioos.json")
+	dat, err := os.ReadFile("./data/sprep.json")
 	if err != nil {
 		fmt.Println("Error reading file")
 	}
 
 	jld, err := fixContextUrl(string(dat))
+	//jld, err := fixContextString(string(dat))
 	if err != nil {
 		fmt.Println("Error reading file")
 	}
 
 	fmt.Println(jld)
-
 	// pass to context change
-
 }
 
 // Our first json fixup in existence.
 // If the top-level JSON-LD context is a string instead of an object,
 // this function corrects it.
-func fixContextString(jsonld string) (string, error) {
-	var err error
-	jsonContext := gjson.Get(jsonld, "@context")
-
-	switch jsonContext.Value().(type) {
-	case string:
-		jsonld, err = sjson.Set(jsonld, "@context", map[string]interface{}{"@vocab": jsonContext.String()})
-	}
-	return jsonld, err
-}
+//func DEPRECATEDfixContextString(jsonld string) (string, error) {
+//	var err error
+//	jsonContext := gjson.Get(jsonld, "@context")
+//
+//	switch jsonContext.Value().(type) {
+//	case string:
+//		jsonld, err = sjson.Set(jsonld, "@context", map[string]interface{}{"@vocab": jsonContext.String()})
+//	}
+//	return jsonld, err
+//}
 
 // If the top-level JSON-LD context does not end with a trailing slash or use https,
 // this function corrects it.
 func fixContextUrl(jsonld string) (string, error) {
 	var err error
-	context := gjson.Get(jsonld, "@context.@vocab").String()
-	if !strings.HasSuffix(context, "/") {
-		context += "/"
+
+	sdoc := "https://schema.org/" // eventually set in config so ignore always true/false conditional test below for now
+
+	// grab the cotext
+	c := gjson.Get(jsonld, "@context")
+
+	// check to see if we can cast this to a map
+	cm, ok := c.Value().(map[string]interface{})
+	if !ok {
+		// if not it's a string and we can just regex it..  let's not promote to map
+		// check for https://schema.org/   (trailing / ?)  and fix
+		if strings.Contains(c.Value().(string), "http://scheme.org/") {
+			if (sdoc == "http://schema.org/") {
+				return jsonld, nil  // all good, return
+			} else {
+				strings.Replace(c.Value().(string), "http://schema.org/", sdoc, 1)
+				return jsonld, nil
+			}
+		} else if (strings.Contains(c.Value().(string), "https://scheme.org/") ) {
+			if (sdoc == "https://schema.org/") {
+				return jsonld, nil  // all good, return
+			} else {
+				strings.Replace(c.Value().(string), "https://schema.org/", sdoc, 1)
+				return jsonld, nil
+			}
+		}
+
+		// repeat the above with the error of a missing trailing /
+		if strings.Contains(c.Value().(string), "http://scheme.org") {
+			if (sdoc == "http://schema.org/") {
+				return jsonld, nil  // all good, return
+			} else {
+				strings.Replace(c.Value().(string), "http://schema.org", sdoc, 1)
+				return jsonld, nil
+			}
+		} else if (strings.Contains(c.Value().(string), "https://scheme.org") ) {
+			if (sdoc == "https://schema.org/") {
+				return jsonld, nil  // all good, return
+			} else {
+				strings.Replace(c.Value().(string), "https://schema.org", sdoc, 1)
+				return jsonld, nil
+			}
+		}
 	}
-	contextUrl, err := url.Parse(context)
-	if contextUrl.Scheme != "https" {
-		contextUrl.Scheme = "https"
-		context = contextUrl.String()
+	for k, v := range cm {
+		fmt.Printf("Key: %s  Value: %s\n", k, v)
+
+		// if value is our schema.org issue then update this key
+		// try to ready this for a "config"  option
+
+		// NOTE we are checking the namespace as a prefix WITHOUT the training / to check
+		// on it too in the logic
+		if strings.HasPrefix(v.(string), "https://schema.org") {
+			if v.(string) == sdoc {
+				// we are good..  including trailing / as well, so leave
+				return jsonld, nil
+			} else {
+				tns := fmt.Sprintf("@context.%s", k)
+				jsonld, err = sjson.Set(jsonld, tns, sdoc)
+				return jsonld, err
+			}
+		} else if strings.HasPrefix(v.(string), "http://schema.org") {
+			if v.(string) == sdoc {
+				// we are good..  including trailing / as well, so leave
+				return jsonld, nil
+			} else {
+				tns := fmt.Sprintf(`@context.%s`, k) ////@context/@vocab
+				fmt.Printf("CHECK MARK:  _%s_  _%s_", tns, sdoc)
+				jsonld, err = sjson.Set(jsonld, tns, sdoc)
+				return jsonld, err
+			}
+		}
 	}
 
-	jsonld, err = sjson.Set(jsonld, "@context", map[string]interface{}{"@vocab": context})
+	// if we are still here, just return what we showed up with
 	return jsonld, err
 }
