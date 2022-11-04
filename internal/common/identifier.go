dee6844d21b0845b30339156bb4d20625b905a16
David Valentine <david.valentine@gmail.com>
2022-11-04 11:23:41 -0700

    Example JSON Paths and arrays of paths

--- internal/common/identifier.go
+++ internal/common/identifier.go
@@ -10,6 +10,7 @@ There are four implementations... so you can see if one might be a little quirky
 */
 import (
 	"errors"
+	"fmt"
 	"github.com/ohler55/ojg/jp"
 	"github.com/ohler55/ojg/oj"
 )
@@ -29,12 +30,35 @@ func GetIdentifierByPath(jsonPath string, jsonld string) (interface{}, error) {
 }
 
 // given a set of json paths return the first to the last.
+/*
+Pass an array of JSONPATH, and get returned the first not empty, result
+Cautions: test your paths, consensus returns [] for a $.identifer.value, even through
+
+{ identifier:"string"}
+has no value:
+
+"idenfitier":
+	{
+	"@type": "PropertyValue",
+	"@id": "https://doi.org/10.1575/1912/bco-dmo.2343.1",
+	"propertyID": "https://registry.identifiers.org/registry/doi",
+	"value": "doi:10.1575/1912/bco-dmo.2343.1",
+	"url": "https://doi.org/10.1575/1912/bco-dmo.2343.1"
+	}
+https://cburgmer.github.io/json-path-comparison/results/dot_notation_on_object_without_key.html
+https://cburgmer.github.io/json-path-comparison/results/dot_notation_on_null_value.html
+*/
 func GetIdentiferByPaths(jsonpaths []string, jsonld string) (interface{}, error) {
 	for _, jsonPath := range jsonpaths {
 		obj, err := GetIdentifierByPath(jsonPath, jsonld)
 		if err != nil {
+
 			continue
 		} else {
+			// sometimes an empty string is returned
+			if fmt.Sprint(obj) == "[]" {
+				continue
+			}
 			return obj, err
 		}
 	}
