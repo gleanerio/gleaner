9520b31fb580d0f60c64eb800c09373570e367a4
melinda <melindaminch@oceannetworks.ca>
2023-01-09 16:22:49 -0800

    Revert bad change

--- internal/summoner/acquire/headlessNG.go
+++ internal/summoner/acquire/headlessNG.go
@@ -238,7 +238,7 @@ func PageRender(v1 *viper.Viper, mc *minio.Client, timeout time.Duration, url, k
 	expression := `
 		function getMetadata() {
 			return new Promise((resolve, reject) => {
-				const elements = document.querySelectorAll('script[type=JSONContentType]');
+				const elements = document.querySelectorAll('script[type=application/ld+json]');
 				let metadata = [];
 				elements.forEach(function(element) {
 					if(element && element.innerText) {
