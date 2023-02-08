5f9bcd145bba232153997e43618da76308725120
David Valentine <david.valentine@gmail.com>
2023-02-08 10:51:13 -0800

    Implement headlessWait in the headless Javascript. Fixes #152 add a test that is expected to fail, with 1 JSONld returned.

--- internal/summoner/acquire/headless_test.go
+++ internal/summoner/acquire/headless_test.go
@@ -18,11 +18,18 @@ func TestHeadlessNG(t *testing.T) {
 		url          string
 		jsonldcount  int
 		headlessWait int
+		expectedFail bool "default:false"
 	}{
-		{name: "r2r_1",
+		{name: "r2r_wait_5_works_returns_2_jsonld",
 			url:          "https://dev.rvdata.us/search/fileset/100135",
 			jsonldcount:  2,
 			headlessWait: 5,
+		},
+		{name: "r2r_expectedfail_wait_0_returns_1_jsonld_fails_if_2_jsonld",
+			url:          "https://dev.rvdata.us/search/fileset/100135",
+			jsonldcount:  2,
+			headlessWait: 0,
+			expectedFail: true,
 		},
 	}
 
@@ -42,8 +49,11 @@ func TestHeadlessNG(t *testing.T) {
 		repoLogger, _ := common.LogIssues(viper, test.name)
 		t.Run(test.name, func(t *testing.T) {
 			jsonlds, err := PageRender(viper, 5*time.Second, test.url, test.name, repoLogger, runstats)
-
-			assert.Equal(t, test.jsonldcount, len(jsonlds))
+			if !test.expectedFail {
+				assert.Equal(t, test.jsonldcount, len(jsonlds))
+			} else {
+				assert.NotEqual(t, test.jsonldcount, len(jsonlds))
+			}
 
 			assert.Nil(t, err)
 
